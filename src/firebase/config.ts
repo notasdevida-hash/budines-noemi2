bac9c72164a3aaf6c71a7f5eec9b9ebcdad11ee9
Notas de vida Kids <notasdevidakids@gmail.com>
2026-02-07 03:40:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2417448560-67a0a",
+  "appId": "1:844137929101:web:50e40f0e0cd688989885c4",
+  "apiKey": "AIzaSyA2q6pGQYtEUijStLexJaBdRvZOU2c15CI",
+  "authDomain": "studio-2417448560-67a0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844137929101"
+};
